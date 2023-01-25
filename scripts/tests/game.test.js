de1116b6049b0ed0866836f979f1e0244e09a7e5
Rob Sizeland <robcakes69@gmail.com>
2023-01-25 13:14:02 +0000

    Add addTurn to allow a random number to be chosen for the computer's turn

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -44,9 +44,9 @@ describe("newGame works correctly", () => {
     test("should set the playerMoves to an empty array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    test("should set the currentGame to an empty array", () => {
-        expect(game.currentGame.length).toBe(0);
-    });
+    test("should be one element in the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+    })
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     })
